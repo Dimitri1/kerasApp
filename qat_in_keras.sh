81d4812cb6c70dd3f9151151f8be9ec37621ec81
Dimitri Gerin <dimitri.gerin@vsora.com>
2020-09-09 17:38:46 +0200

    [DEV] add .sh for req

---
+++ qat_in_keras.sh
@@ -0,0 +1,11 @@
+pip uninstall -y tensorflow tensorflow-gpu
+pip install -q tf-nightly-gpu
+pip install -q tensorflow-model-optimization
+
+pip show tf-nightly-gpu
+
+pip show tensorflow-model-optimization
+
+nvidia-smi
+pip install matplotlib
+pip install tensorflow_datasets
