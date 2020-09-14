47b58ee982a2a3661f854b3898bb7b06096d84c3
Dimitri Gerin <dimitri.gerin@vsora.com>
2020-09-14 16:06:43 +0200

    using tf.keras on tf 2.2

--- install_pip_packages.sh
+++ install_pip_packages.sh
@@ -1,4 +1,4 @@
-$1/bin/pip uninstall -y tensorflow tensorflow-gpu
+$1/bin/pip uninstall -y tensorflow tensorflow-gpu keras
 $1/bin/pip install -q tf-nightly-gpu
 $1/bin/pip install -q tensorflow-model-optimization
 
@@ -9,5 +9,4 @@ nvidia-smi
 $1/bin/pip install matplotlib
 $1/bin/pip install tensorflow_datasets
 
-$1/bin/pip install tsensorflow=='1.8.0'
-$1/bin/pip install keras=='2.2.5'
+$1/bin/pip install tsensorflow=='2.3'
